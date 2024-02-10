fd890f72343c4c7c10d0a015aa421d455e9e199d
Chukwuka Eze <88329191+kenchuks44@users.noreply.github.com>
2024-02-10 06:23:23 +0100

    Update main.tf

--- jenkins-server/main.tf
+++ jenkins-server/main.tf
@@ -5,7 +5,7 @@ module "vpc" {
   name = "jenkins-vpc"
   cidr = var.vpc_cidr
 
-  azs            = data.aws_availability_zones.azs.names
+  azs = data.aws_availability_zones.azs.names
   public_subnets = var.public_subnets
 
   enable_dns_hostnames = true
